f2c482ad72a5cdf0a02166bf98fe35d0be717c8d
anika-codes <82987182+anika-codes@users.noreply.github.com>
2023-08-28 19:04:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var prediction1 = "";
+var prediction2 = "";
+
 Webcam.set({ 
     width: 350,
     height: 300,
@@ -19,3 +22,81 @@ console.log("ML5 loaded version is ", ml5.version);
 
 
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iYawB4uEP/model.json", model_loaded);
+
+function model_loaded(){
+    console.log("My model is loaded");
+}
+
+function agent_speak(){
+    var speak_agent = window.speechSynthesis;
+    var my_data = "Prediction one is:" + prediction1 + " and Prediction two is: " + prediction2;
+    var uttering = new SpeechSynthesisUtterance(my_data);
+    speak_agent.speak(uttering);
+
+}
+
+function predict_hand_sign(){
+    image = document.getElementById("image_result");
+    classifier.classify(image, got_results);
+}
+
+function got_results(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        document.getElementById("prediction_1_name").innerHTML = prediction1;
+        document.getElementById("prediction_2_name").innerHTML = prediction2;
+
+        if(prediction1 == "Thumbs Up"){
+            document.getElementById("prediction_1_emoji").innerHTML = "&#128077;";
+
+        }
+        if(prediction1 == "OK Sign"){
+            document.getElementById("prediction_1_emoji").innerHTML = "&#128076;";
+            
+        }
+        if(prediction1 == "Thumbs Down"){
+            document.getElementById("prediction_1_emoji").innerHTML = "&#128078;";
+            
+        }
+        if(prediction1 == "Heart"){
+            document.getElementById("prediction_1_emoji").innerHTML = "&#128080;";
+            
+        }
+        if(prediction1 == "Peace Sign"){
+            document.getElementById("prediction_1_emoji").innerHTML = "&#9996;";
+            
+        }
+
+        if(prediction2 == "Happy"){
+            document.getElementById("prediction_2_emoji").innerHTML = "&#128513;";
+
+        }
+        if(prediction2 == "Sad"){
+            document.getElementById("prediction_2_emoji").innerHTML = "&#128557;";
+            
+        }
+        if(prediction2 == "Calm"){
+            document.getElementById("prediction_2_emoji").innerHTML = "&#128524;";
+            
+        }
+        if(prediction2 == "Mad"){
+            document.getElementById("prediction_2_emoji").innerHTML = "&#128544;";
+            
+        }
+        if(prediction2 == "Surprised"){
+            document.getElementById("prediction_2_emoji").innerHTML = "&#128562;";
+            
+        }
+
+        agent_speak();
+
+        
+    }
+}
+
+
